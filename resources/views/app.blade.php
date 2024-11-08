2a1772884c40ab0063a97ff947548894b957c365
Lugaya <lugayamuktias@gmail.com>
2024-11-08 16:59:35 +0700

    masih coba url debugging vercel.cobaremove

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,6 @@
     <link rel="icon" href="{{ asset('new-favicon.ico') }}" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     @viteReactRefresh
-    @vite('resources/js/app.jsx')
     @inertiaHead
 </head>
 <body>
